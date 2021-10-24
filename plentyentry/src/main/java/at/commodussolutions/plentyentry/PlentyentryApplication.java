ab440ca96c93b237d5a6bc59726d098c45a825cf
sabrieker <ekersabri1050@hotmail.com>
2021-10-24 22:29:31 +0200

    first test

---
+++ plentyentry/src/main/java/at/commodussolutions/plentyentry/PlentyentryApplication.java
@@ -0,0 +1,13 @@
+package at.commodussolutions.plentyentry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlentyentryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlentyentryApplication.class, args);
+    }
+
+}
---
+++ plentyentry/src/test/java/at/commodussolutions/plentyentry/PlentyentryApplicationTests.java
@@ -0,0 +1,13 @@
+package at.commodussolutions.plentyentry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlentyentryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
