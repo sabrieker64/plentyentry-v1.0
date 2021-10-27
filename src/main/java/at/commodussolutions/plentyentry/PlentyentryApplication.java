0d0fa7c56232e86aa495822cb39429d9e6953bfc
sabrieker <ekersabri1050@hotmail.com>
2021-10-27 19:15:09 +0200

    first test

--- src/main/java/at/commodussolutions/plentyentry/PlentyentryApplication.java
+++ src/main/java/at/commodussolutions/plentyentry/PlentyentryApplication.java
@@ -2,12 +2,22 @@ package at.commodussolutions.plentyentry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class PlentyentryApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PlentyentryApplication.class, args);
+    }
+
+    @GetMapping("/hello")
+    @ResponseBody
+    public String sayHello(){
+        return "Hello";
     }
 
 }
