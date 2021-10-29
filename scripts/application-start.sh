b8e05513cbb811c207de1fb474f2f95d99ea5524
sabrieker <ekersabri1050@hotmail.com>
2021-10-29 15:44:50 +0200

    test

--- scripts/application-start.sh
+++ scripts/application-start.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-cd /tmp/|| exit
-java -jar target/plentyentry-application.jar
+java -jar ../target/plentyentry-application.jar
 echo "The ApplicationStart deployment lifecycle event successfully completed." > application-start.txt
