8155fae4fc483020c624486eed800aaf002b3213
sabrieker <ekersabri1050@hotmail.com>
2021-11-06 17:03:45 +0100

    frontend impl

--- scripts/application-stop.sh
+++ scripts/application-stop.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+chmod u+x/tmp/plentyentry-application.jar
 service plentyentry-application stop
+echo "The ApplicationStart deployment lifecycle event successfully completed." > application-start.txt
