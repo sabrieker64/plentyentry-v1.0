b7c47ec7d934b9cddc373e49d30c4ef9ac0aec03
sabrieker <ekersabri1050@hotmail.com>
2021-11-06 12:30:07 +0100

    update

--- scripts/application-start.sh
+++ scripts/application-start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 java -jar /tmp/plentyentry-application.jar
-ng serve
+ng serve --port 8081
 echo "The ApplicationStart deployment lifecycle event successfully completed." > application-start.txt
 
