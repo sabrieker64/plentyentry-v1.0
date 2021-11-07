32b62e5e5149edc3df68d82c30d12f5ea01d3224
sabrieker <ekersabri1050@hotmail.com>
2021-11-07 22:50:53 +0100

    ultimate update

--- scripts/validate-service.sh
+++ scripts/validate-service.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-chmod u+x/tmp/plentyentry-application.jar
-service plentyentry-application start
-echo "Validating Service, Application Start Success" > validate-service.txt
+
+status_code=$(curl --write-out %{http_code} --silent --output /dev/null ec2-3-144-231-140.us-east-2.compute.amazonaws.com:8080/hello)
+
+if [[ "$status_code" -ne 200 ]] ; then
+  echo "Site status changed to $status_code" | mail -s "SITE STATUS CHECKER" "ekersabri1050@hotmail.com" -r "STATUS_CHECKER"
+else
+  exit 0
+fi
