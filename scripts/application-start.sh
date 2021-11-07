d1f4ef612493a3b32b135622bb3d1f63c669929d
sabrieker <ekersabri1050@hotmail.com>
2021-11-08 00:13:56 +0100

    update

--- scripts/application-start.sh
+++ scripts/application-start.sh
@@ -2,4 +2,3 @@
 chmod u+x/tmp/plentyentry-application.jar
 service plentyentry-application start
 echo "The ApplicationStart deployment lifecycle event successfully completed." > application-start.txt
-
