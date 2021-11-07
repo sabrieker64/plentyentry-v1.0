46c90454bc32f97a9c7382c4e1885fe2db5cdd41
sabrieker <ekersabri1050@hotmail.com>
2021-11-08 00:21:56 +0100

    finished

--- scripts/validate-service.sh
+++ scripts/validate-service.sh
@@ -3,7 +3,7 @@
 status_code=$(curl --write-out %{http_code} --silent --output /dev/null ec2-3-144-231-140.us-east-2.compute.amazonaws.com:8080/hello)
 
 if [[ "$status_code" -ne 200 ]] ; then
-  echo "Site status changed to $status_code" | mail -s "SITE STATUS CHECKER" "ekersabri1050@hotmail.com" -r "STATUS_CHECKER"
+  echo "Site status changed to $status_code"
 else
   exit 0
 fi
