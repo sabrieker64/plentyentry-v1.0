1c2c834212c15bc3b801ec84357e095f8ab96516
sabrieker <ekersabri1050@hotmail.com>
2021-11-11 22:28:50 +0100

    update deploy script

--- scripts/application-start.sh
+++ scripts/application-start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-nohup java -jar ../target/plentyentry-application.jar > application-start.txt
+java -jar /opt/codedeploy-agent/deployment-root/bea34b85-d2c6-4bec-bd32-31dbadd0a20d/d-T4BZG7WXC/deployment-archive/target/plentyentry-application.jar
 echo $! > ../target/pid.file
