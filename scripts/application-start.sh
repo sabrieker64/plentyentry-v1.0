1d4c46ba4b75b03f03afc5006ad4be07b02eb943
sabrieker <ekersabri1050@hotmail.com>
2021-11-17 22:01:13 +0100

    update

--- scripts/application-start.sh
+++ scripts/application-start.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 java -jar /opt/codedeploy-agent/deployment-root/bea34b85-d2c6-4bec-bd32-31dbadd0a20d/d-T4BZG7WXC/deployment-archive/target/plentyentry-application.jar > application-start.txt
+plentyentry-frontend/npm start
