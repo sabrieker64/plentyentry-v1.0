e815abef5e4eb5ca3bcbdc6854ecda5679294313
sabrieker <ekersabri1050@hotmail.com>
2021-12-14 23:28:03 +0100

    buildspec options

--- scripts/application-start.sh
+++ scripts/application-start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
+java -jar /target/plentyentry-application.jar > /dev/null 2>&1 &
 ls
 cd ../
-java -jar /target/plentyentry-application.jar > /dev/null 2>&1 &
 cd ../plentyentry-frontend || exit
 ng serve --port 4200
