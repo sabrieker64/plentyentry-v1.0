1d0b2867751859252849ec9a2a2adf2f08882739
sabrieker <ekersabri1050@hotmail.com>
2021-12-16 00:23:37 +0100

    starting servers

--- scripts/application-start.sh
+++ scripts/application-start.sh
@@ -3,5 +3,5 @@ ls
 echo Starting Spring Boot Backend Application
 java -jar /target/plentyentry-application.jar > /dev/null 2>&1 &
 echo Starting Frontend at Port 4200
-cd ../deployment-root/bea34b85-d2c6-4bec-bd32-31dbadd0a20d/d-IX9W57EKD/deployment-archive/plentyentry-frontend || exit
+cd plentyentry-frontend
 ng serve --port 4200
