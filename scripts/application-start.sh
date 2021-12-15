a85160254f6692ba04bf68e17ae6cb8cddbf35f3
sabrieker <ekersabri1050@hotmail.com>
2021-12-15 23:55:44 +0100

    test

--- scripts/application-start.sh
+++ scripts/application-start.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 java -jar /target/plentyentry-application.jar > /dev/null 2>&1 &
+echo First
 ls
 cd ../
+echo Second
 ls
 cd ../
+echo Third
 ls
 cd plentyentry-frontend || exit
 ng serve --port 4200
