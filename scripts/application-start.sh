7f0feae08c8130fd9174a749ecbe6211fcc1c0af
sabrieker <ekersabri1050@hotmail.com>
2021-12-17 17:10:56 +0100

    starting servers and vallidate service for pipeline

--- scripts/application-start.sh
+++ scripts/application-start.sh
@@ -3,5 +3,5 @@ ls
 echo Starting Spring Boot Backend Application
 java -jar /target/plentyentry-application.jar > /dev/null 2>&1 &
 echo Starting Frontend at Port 4200
-cd plentyentry-frontend/dist || exit
+cd plentyentry-frontend || exit
 ng serve --port 4200
