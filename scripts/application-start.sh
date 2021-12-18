66acd30345bb1b10adfc89ddcd3725676bf652d6
sabrieker <ekersabri1050@hotmail.com>
2021-12-18 20:52:18 +0100

    deployment error fix

--- scripts/application-start.sh
+++ scripts/application-start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 java -jar backend/jar/target/plentyentry-application.jar > /dev/null 2>&1 &
 echo Started Spring Boot Backend Application
-cd frontend/dist || exit
+cd ../plentyentry-frontend/dist || exit
 ng serve --port 4200
 echo Started Frontend at Port 4200
