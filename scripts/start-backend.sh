50ab395b5936272af8a27ba3d35be024b8b1e4a3
sabrieker <ekersabri1050@hotmail.com>
2021-12-20 19:09:40 +0100

    deployment error fix

--- scripts/start-backend.sh
+++ scripts/start-backend.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-java -jar ../target/plentyentry-application.jar
+java -jar ../target/plentyentry-application.jar > backend-start-log.txt
 echo Started Spring Boot Backend Application at port 8080
 
