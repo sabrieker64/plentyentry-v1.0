6d7d2e23c772f9a3559780f40f10d9592059b51c
sabrieker <ekersabri1050@hotmail.com>
2021-12-24 10:59:10 +0100

    frontend update

--- plentyentry-frontend/src/app/event-tile/event-tile-routing.module.ts
+++ plentyentry-frontend/src/app/event-tile/event-tile-routing.module.ts
@@ -1,7 +1,20 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
+import {EventTileOverviewComponent} from "./event-tile-overview/event-tile-overview.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: ' ',
+    pathMatch: 'full',
+    redirectTo: 'overview'
+  },
+
+  {
+    path: 'overview',
+    component: EventTileOverviewComponent,
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
