e253055c0d3ec8c0d180a01adcba21f5510554e5
sabrieker <ekersabri1050@hotmail.com>
2022-01-02 14:44:20 +0100

    updated routing modules and db changes

--- scripts/start-backend.sh
+++ scripts/start-backend.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-java -jar target/plentyentry-application.jar > backend-start-log.txt
+java -jar ../target/plentyentry-application.jar > backend-start-log.txt
 echo Started Spring Boot Backend Application at port 8080
 
--- scripts/start-frontend.sh
+++ scripts/start-frontend.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-npm start --prefix plentyentry-frontend  > frontend-start-log.txt
+npm start --prefix ../plentyentry-frontend  > frontend-start-log.txt
 echo Started Frontend at Port 4200
