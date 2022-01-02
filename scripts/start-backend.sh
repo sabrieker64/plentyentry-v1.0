8ead6d42746574bfe8224d59f95668d06e8fcdbe
sabrieker <ekersabri1050@hotmail.com>
2022-01-02 19:36:37 +0100

    updated routing modules and db changes

--- scripts/start-backend.sh
+++ scripts/start-backend.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 java -jar ../target/plentyentry-application.jar
+cd ../plentyentry-frontend || exit
+npm start --prefix ../plentyentry-frontend
 echo Started Spring Boot Backend Application at port 8080
-
