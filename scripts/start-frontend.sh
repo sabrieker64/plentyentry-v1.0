9eddd0e6ddf6ba12307434872487466508053f17
sabrieker <ekersabri1050@hotmail.com>
2022-01-04 19:38:40 +0100

    deplymnen error

--- scripts/start-frontend.sh
+++ scripts/start-frontend.sh
@@ -2,5 +2,5 @@
 cd /home/ec2-user/frontend || exit
 npm i @angular/cli
 npm install
-ng serve --port 4200 > /dev/null 2> /dev/null < /dev/null &
+npm start > /dev/null 2> /dev/null < /dev/null &
 echo Started Frontend at Port 4200
