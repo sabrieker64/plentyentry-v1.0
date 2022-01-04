3844c801591405567c471fbb248eee48b502f4a8
sabrieker <ekersabri1050@hotmail.com>
2022-01-04 15:22:52 +0100

    deplymnen error

--- scripts/start-backend.sh
+++ scripts/start-backend.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cd /home/ec2-user
+cd /home/ec2-user/target || exit
 java -jar -Dspring.profiles.active=qa plentyentry-application.jar > /dev/null 2> /dev/null < /dev/null &
--- scripts/start-frontend.sh
+++ scripts/start-frontend.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cd /home/ec2-user
+cd /home/ec2-user/frontend || exit
 ng serve --port 4200 > /dev/null 2> /dev/null < /dev/null &
 echo Started Frontend at Port 4200
