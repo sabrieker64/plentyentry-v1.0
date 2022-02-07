e7f5add2f5bf1a6a3eba12e479c61f95d187091e
sabrieker <ekersabri1050@hotmail.com>
2022-02-07 21:41:20 +0100

    fix pipeline

--- plentyentry-frontend/src/app/definitions/objects.ts
+++ plentyentry-frontend/src/app/definitions/objects.ts
@@ -1,6 +1,6 @@
 /* tslint:disable */
 /* eslint-disable */
-// Generated using typescript-generator version 2.32.889 on 2022-02-07 21:29:17.
+// Generated using typescript-generator version 2.32.889 on 2022-02-07 21:40:38.
 
 export interface UserDTO {
   id: number;
