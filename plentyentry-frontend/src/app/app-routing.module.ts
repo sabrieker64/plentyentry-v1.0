e7b06d4bf1c4af8001733fa94ae3730c35e8b578
sabrieker <ekersabri1050@hotmail.com>
2022-02-17 19:48:38 +0100

    fix refresh error

--- plentyentry-frontend/src/app/app-routing.module.ts
+++ plentyentry-frontend/src/app/app-routing.module.ts
@@ -18,7 +18,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
+  imports: [RouterModule.forChild(routes), RouterModule.forRoot(routes, {useHash: true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule {
