ec47752dcf3438aa07cb31c726fe7d7039d6570e
sabrieker <ekersabri1050@hotmail.com>
2022-04-06 22:34:34 +0200

    fixed pipeline

--- plentyentry-frontend/src/app/payment/service/payment.service.ts
+++ plentyentry-frontend/src/app/payment/service/payment.service.ts
@@ -1,8 +1,8 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {environment} from "../../../environments/environment";
-import {CreatePayment, CreatePaymentResponse} from "../../definitions/objects";
 import {Observable} from "rxjs";
+import {CreatePayment, CreatePaymentResponse} from "../../definitions/objects";
 
 @Injectable({
   providedIn: 'root'
