d6b43e956e262b856f61aec91235821fca960233
sabrieker <ekersabri1050@hotmail.com>
2022-04-09 16:23:14 +0200

    optimize batch jobs

--- scripts/after-install.sh
+++ scripts/after-install.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-sudo chmod +x /home/ec2-user/server/plentyentry-application.jar
-sudo chmod +x /home/ec2-user/server/start-backend.sh
-echo "The AfterInstall deployment lifecycle event successfully completed." > after-install.txt
+chmod +x /home/ec2-user/server/plentyentry-application.jar
+chmod +x /home/ec2-user/server/start-backend.sh
+echo "The AfterInstall deployment lifecycle event successfully completed.";
