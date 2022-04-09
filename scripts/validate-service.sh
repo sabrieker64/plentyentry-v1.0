1446c8df4cc5a02bd83f37782b8f0486acc66181
sabrieker <ekersabri1050@hotmail.com>
2022-04-09 10:34:30 +0200

    make the right validation on backend call //should be 200ok if the server has started probarly

--- scripts/validate-service.sh
+++ scripts/validate-service.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-status_code=$(curl --write-out %{http_code} --silent --output /dev/null ec2-3-144-231-140.us-east-2.compute.amazonaws.com:8080/hello)
+status_code=$(curl --write-out %{http_code} --silent --output /dev/null ec2-3-144-231-140.us-east-2.compute.amazonaws.com:8080/api/backend/event/list)
 
 if [[ "$status_code" -ne 200 ]] ; then
   echo "Site status changed to $status_code"
