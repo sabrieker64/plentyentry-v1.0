af0054ac28f3b8843d8cfe77b0bfa4912b8b536b
sabrieker <ekersabri1050@hotmail.com>
2022-05-04 23:35:10 +0200

    fixing versions

--- plentyentry-frontend/src/app/payment/stripe/stripe.service.ts
+++ plentyentry-frontend/src/app/payment/stripe/stripe.service.ts
@@ -1,5 +1,4 @@
 import {Injectable} from '@angular/core';
-import {Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
@@ -11,16 +10,4 @@ export class StripeService {
   constructor() {
   }
 
-  public initializeStripe() {
-    return new Observable((observer) => {
-      const script = document.createElement('script');
-      script.type = 'text/javascript';
-      script.src = this.stripejsUrl;
-      script.onload = () => {
-        observer.next();
-        observer.complete();
-      };
-      document.getElementsByTagName('head')[0].appendChild(script);
-    });
-  }
 }
