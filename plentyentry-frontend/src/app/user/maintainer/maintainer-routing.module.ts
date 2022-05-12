569b8954b0a5d864291df5194ecd65bda3212d77
sabrieker <ekersabri1050@hotmail.com>
2022-05-12 22:45:07 +0200

    update

--- plentyentry-frontend/src/app/user/maintainer/maintainer-routing.module.ts
+++ plentyentry-frontend/src/app/user/maintainer/maintainer-routing.module.ts
@@ -1,9 +1,9 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 import {MaintainedEventsListComponent} from "./maintained-events-list/maintained-events-list.component";
-import {MaintainedEventScanComponent} from "./maintained-event-scan/maintained-event-scan.component";
 import {MaintainedEventEditComponent} from "./maintained-event-edit/maintained-event-edit.component";
 import {MaintainedEventCreateComponent} from "./maintained-event-create/maintained-event-create.component";
+import {MaintainedEventScanComponent} from "./maintained-event-scan/maintained-event-scan.component";
 
 const routes: Routes = [
 
--- plentyentry-frontend/src/app/user/maintainer/maintainer.module.ts
+++ plentyentry-frontend/src/app/user/maintainer/maintainer.module.ts
@@ -1,13 +1,13 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 
-import { MaintainerRoutingModule } from './maintainer-routing.module';
-import { MaintainedEventsListComponent } from './maintained-events-list/maintained-events-list.component';
+import {MaintainerRoutingModule} from './maintainer-routing.module';
+import {MaintainedEventsListComponent} from './maintained-events-list/maintained-events-list.component';
 import {AngularMaterialModule} from "../../../library/anguler-material-module/anguler-material-module.module";
-import { MaintainedEventScanComponent } from './maintained-event-scan/maintained-event-scan.component';
-import { MaintainedEventEditComponent } from './maintained-event-edit/maintained-event-edit.component';
+import {MaintainedEventEditComponent} from './maintained-event-edit/maintained-event-edit.component';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-import { MaintainedEventCreateComponent } from './maintained-event-create/maintained-event-create.component';
+import {MaintainedEventCreateComponent} from './maintained-event-create/maintained-event-create.component';
+import {MaintainedEventScanComponent} from "./maintained-event-scan/maintained-event-scan.component";
 
 
 @NgModule({
