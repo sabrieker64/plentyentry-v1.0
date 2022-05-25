3f46000965a94fb44c9f4f3b409e3cf476a66d77
sabrieker <ekersabri1050@hotmail.com>
2022-05-25 17:22:58 +0200

    our own security annotation //please contact me if you dont understand it

--- src/main/java/at/commodussolutions/plentyentry/backendConfig/utils/PESecured.java
+++ src/main/java/at/commodussolutions/plentyentry/backendConfig/utils/PESecured.java
@@ -1,28 +1,16 @@
 package at.commodussolutions.plentyentry.backendConfig.utils;
 
-import at.commodussolutions.plentyentry.user.authentication.constant.SecurityConstant;
 import at.commodussolutions.plentyentry.user.userdata.enums.UserType;
 
-import javax.validation.Constraint;
-import javax.validation.Payload;
-import java.lang.annotation.Documented;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import java.lang.annotation.*;
 
-import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.TYPE;
 
-@Target({FIELD, ANNOTATION_TYPE, TYPE_USE, METHOD, CONSTRUCTOR})
+@Target({METHOD, TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
-@Constraint(validatedBy = PESecuredValidator.class)
+@Inherited
 public @interface PESecured {
-
-    String message() default SecurityConstant.ACCESS_DENIED_MESSAGE;
-
-    Class<? extends Payload>[] payload() default {};
-
     UserType[] value();
-
-
 }
