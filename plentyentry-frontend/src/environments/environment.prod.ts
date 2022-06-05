0564ea1ea14e2eed4872f62cc1f63999bd2761b9
sabrieker <ekersabri1050@hotmail.com>
2022-06-05 12:48:12 +0200

    activated actuator

--- plentyentry-frontend/src/environments/environment.prod.ts
+++ plentyentry-frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   trackingEnabled: true,
-  baseUrl: 'http://ec2-18-188-57-68.us-east-2.compute.amazonaws.com:8080/',
+  baseUrl: 'http://18.222.4.37:8080/',
   stripe: 'pk_nochkeinkey'
 };
