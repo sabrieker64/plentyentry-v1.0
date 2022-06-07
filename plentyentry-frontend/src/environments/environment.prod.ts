093f0754157711cd6c0dddcfe4de934aef9f056b
sabrieker <ekersabri1050@hotmail.com>
2022-06-07 20:55:52 +0200

    fixed angular verison

--- plentyentry-frontend/src/environments/environment.prod.ts
+++ plentyentry-frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   trackingEnabled: true,
-  baseUrl: 'http://18.222.4.37:80/',
+  baseUrl: 'https://plenty-entry-backend.com/',
   stripe: 'pk_nochkeinkey'
 };
